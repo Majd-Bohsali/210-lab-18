f401f2aaecf9ac9f0666c5fa3daf6b2b8f7b5225
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-10-04 14:52:12 -0700

    displays average

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -23,34 +23,38 @@ int main() {
     string comment;
     string enterNewReview = "y";
 
-    while (enterNewReview == "y" || enterNewReview == "Y") {
-        cout << "Which linked list method should we use?" << endl 
-            << "    [1] New nodes are added at the head of the linked list" << endl 
-            << "    [2] New nodes are added at the tail of the linked list" << endl
-            << "    Choice: "; 
+     cout << "Which linked list method should we use?" << endl 
+        << "    [1] New nodes are added at the head of the linked list" << endl 
+        << "    [2] New nodes are added at the tail of the linked list" << endl
+        << "    Choice: "; 
+    cin >> choiceSelected; 
+    
+    while (choiceSelected != 1 && choiceSelected != 2) { 
+        cin.clear(); 
+        cin.ignore(ignoreCount, '\n');
+        cout << "Error, please select 1 or 2: "; 
         cin >> choiceSelected; 
-        
-        while (choiceSelected != 1 && choiceSelected != 2) { 
-            cin.clear(); 
-            cin.ignore(ignoreCount, '\n');
-            cout << "Error, please select 1 or 2: "; 
-            cin >> choiceSelected; 
-        }
-        cout << "Enter review rating: ";
+    }
+
+    while (enterNewReview == "y" || enterNewReview == "Y") {
+        cout << "Enter review rating 0-5: ";
         cin >> rating; 
-        cout << "Enter review rating: "; 
-        cin >> comment; 
+        cin.ignore(ignoreCount, '\n');
+        cout << "Enter review comments: "; 
+        getline(cin, comment);
         
         if(choiceSelected == 1) { 
-            addToHead(head, rating, comment); 
+            head = addToHead(head, rating, comment); 
         }
         else if(choiceSelected == 2) { 
-            addToTail(head, rating, comment); 
+            head = addToTail(head, rating, comment); 
         }
 
         cout << "Enter another review? Y/N: "; 
         cin >> enterNewReview; 
     }
+
+    output(head); 
 
     return 0;
 }
@@ -94,10 +98,13 @@ void output(Node* hd) {
         return;
     }
     Node* current = hd;
-    int reviewNum = 1; 
+    int reviewNum = 0; 
+    float totalRatingScore = 0.0; 
     while (current) {
-        cout << "    > Review #" << reviewNum << ": " << current->rating << ": " << current->comments; 
+        cout << "    > Review #" << reviewNum++ << ": " << current->rating << ": " << current->comments << endl; 
+        totalRatingScore += current->rating; 
         current = current->next;
     }
+    cout << "    > Average: " << totalRatingScore/reviewNum;
     cout << endl;
 }
