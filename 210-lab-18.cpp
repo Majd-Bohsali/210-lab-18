7be2e67632cb832c4050802eee25dbe07e7f7164
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-10-04 14:09:07 -0700

    defined Node

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -2,7 +2,14 @@
 // IDE used: Visual Studio Code
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Node {
+    string comments; 
+    float rating; 
+    Node *next;
+};
 
 int main() {
     return 0;
